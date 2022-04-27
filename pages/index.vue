45fb06e6d719ed25fed7696509342e9fe375d8d2
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-04-26 20:13:49 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+const state = reactive({
+    sites: [
+        {
+            title: 'Vue.js',
+            url: 'https://vuejs.org/',
+            results: ['v3.2.33'],
+            lastChecked: '2020-01-01',
+        }
+    ]
+})
+
+</script>
+
+<template>
+<div class="bg-gray-800 text-white h-screen">
+    <header class="w-full h-32 text-center py-5">
+        <h1 class="text-4xl"> Version Checker</h1>
+    </header>
+    <main>
+        <!-- content here -->
+    </main>
+</div>
+</template>
