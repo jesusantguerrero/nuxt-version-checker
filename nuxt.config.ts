2bdc24541ae33cf231182f25b0578858256b9117
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-10-19 11:30:58 -0400

    fb hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ['@/assets/styles/main.css'],
+    nitro: {
+        preset: "firebase"
+    },
     postcss:{
         plugins: {
             tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,5 @@
-<script setup>
-const { data, refresh } = await useAsyncData('sites', () => $fetch('/api/sites'))
-
-const isAdding = ref(false)
-const toggleAdd = () => {
-    isAdding.value = !isAdding.value
-}
-
-const onSubmit = (site) => {
-    const body = {
-        title: site.title,
-        url: site.url,
-        selector: site.selector,
-        actions: [
-            {
-                action: site.action,
-                value: site.value,
-                index: site.index
-            }
-        ]
-    }
-    $fetch('/api/sites', {
-        method: 'POST',
-        body,
-    }).then(() => refresh())
-}
-
-const onCheck = () => {
-    state.sites.forEach(site => {
-        site.results.push('v3.2.33')
-    })
-}
-
-</script>
-
 <template>
-<div class="bg-gray-800 text-white h-screen">
-    <header class="w-full h-32 text-center py-5">
-        <h1 class="text-4xl"> Version Checker</h1>
-    </header>
-    <main class="max-w-6xl mx-auto">
-        <SiteForm v-if="isAdding" @submit="onSubmit" @cancel="toggleAdd" class="mb-10 bg-gray-700 rounded-md" />
-        <SiteTable :sites="data" @add="toggleAdd" @check="onCheck" />
-    </main>
+<div class="h-screen text-white bg-gray-800">
+    Hello mom
 </div>
 </template>
